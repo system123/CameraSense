5251590db1db55921fbe0cef63ba543fcb0a47cb
Lloyd Hughes <hughes.lloyd@gmail.com>
2013-05-16 15:14:58 +0200

    GPUImage framework
    Kept a seperate copy so it doesn't affect my current copy when I update
    things in it.

---
+++ GPUImage/framework/Source/GPUImageRGBFilter.m
@@ -0,0 +1,89 @@
+#import "GPUImageRGBFilter.h"
+
+#if TARGET_IPHONE_SIMULATOR || TARGET_OS_IPHONE
+NSString *const kGPUImageRGBFragmentShaderString = SHADER_STRING
+(
+ varying highp vec2 textureCoordinate;
+ 
+ uniform sampler2D inputImageTexture;
+ uniform highp float red;
+ uniform highp float green;
+ uniform highp float blue;
+ 
+ void main()
+ {
+     highp vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);
+     
+     gl_FragColor = vec4(textureColor.r * red, textureColor.g * green, textureColor.b * blue, 1.0);
+ }
+);
+#else
+NSString *const kGPUImageRGBFragmentShaderString = SHADER_STRING
+(
+ varying vec2 textureCoordinate;
+ 
+ uniform sampler2D inputImageTexture;
+ uniform float red;
+ uniform float green;
+ uniform float blue;
+ 
+ void main()
+ {
+     vec4 textureColor = texture2D(inputImageTexture, textureCoordinate);
+     
+     gl_FragColor = vec4(textureColor.r * red, textureColor.g * green, textureColor.b * blue, 1.0);
+ }
+ );
+#endif
+
+@implementation GPUImageRGBFilter
+
+@synthesize red = _red, blue = _blue, green = _green;
+
+#pragma mark -
+#pragma mark Initialization and teardown
+
+- (id)init;
+{
+    if (!(self = [super initWithFragmentShaderFromString:kGPUImageRGBFragmentShaderString]))
+    {
+		return nil;
+    }
+    
+    redUniform = [filterProgram uniformIndex:@"red"];
+    self.red = 1.0;
+    
+    greenUniform = [filterProgram uniformIndex:@"green"];
+    self.green = 1.0;
+    
+    blueUniform = [filterProgram uniformIndex:@"blue"];
+    self.blue = 1.0;
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark Accessors
+
+- (void)setRed:(CGFloat)newValue;
+{
+    _red = newValue;
+    
+    [self setFloat:_red forUniform:redUniform program:filterProgram];
+}
+
+- (void)setGreen:(CGFloat)newValue;
+{
+    _green = newValue;
+
+    [self setFloat:_green forUniform:greenUniform program:filterProgram];
+}
+
+- (void)setBlue:(CGFloat)newValue;
+{
+    _blue = newValue;
+
+    [self setFloat:_blue forUniform:blueUniform program:filterProgram];
+}
+
+@end
